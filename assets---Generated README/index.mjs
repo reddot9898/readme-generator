1783df5c0c504739a24bb731b887ae31a6e6ce18
randyn.collins@icloud.com <you@example.com>
2023-07-24 18:46:29 -0400

    final code ready to go, no errors fully functional and working

--- assets---Generated README/index.mjs
+++ assets---Generated README/index.mjs
@@ -1,4 +1,9 @@
-// 
+// need the packages for the app, npm init, and npm install inquirer
+// need an array of questions for user input
+// need a function for the readme file
+// need a function to call on to start the application
+// use the readme guide to set up the readme layout
+// need the table of contents to be linked to there respective sections for larger readme's so users can navigate easily
 
 
 
@@ -12,9 +17,12 @@
 import fs from 'fs';
 import inquirer from 'inquirer';
 
-
+// this function will generate your pre-made template readme and add in whatever the user inputs 
 function generateREADME(answers) {
-  return `# ${answers.title}
+  return `
+
+  # Title
+  ${answers.title}
 
 ## Description
 ${answers.description}
@@ -50,23 +58,23 @@ For any questions, please contact me:
 `;
 }
 
-// Function to prompt user for input
+// function running the application, what you'll actually be asked in the terminal
 async function promptUser() {
   const answers = await inquirer.prompt([
     {
       type: 'input',
       name: 'title',
-      message: 'Enter the title of your project:',
+      message: 'What Is The Title Of Your Project ?:',
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Enter a description for your project:',
+      message: 'Enter Your description For Your Project:',
     },
     {
       type: 'input',
       name: 'installation',
-      message: 'Enter installation instructions:',
+      message: 'Enter Installation Instructions For Your Project:',
     },
     {
       type: 'input',
@@ -76,7 +84,7 @@ async function promptUser() {
     {
       type: 'list',
       name: 'license',
-      message: 'Choose a license for your application:',
+      message: 'What License Will Your Project Be Using ?:',
       choices: ['MIT', 'Apache 2.0', 'GNU GPLv3', 'ISC', 'Unlicense', 'Other'],
     },
     {
@@ -92,25 +100,25 @@ async function promptUser() {
     {
       type: 'input',
       name: 'username',
-      message: 'Enter your GitHub username:',
+      message: 'What Is Your GitHub Username ?:',
     },
     {
       type: 'input',
       name: 'email',
-      message: 'Enter your email address:',
+      message: 'What Is Your Email Address ?:',
     },
   ]);
 
   return answers;
 }
 
-
+// this section of code i cannot take credit for, i was receiving a lot of errors running it and eventually had to ask what in the world i was doing wrong
+// this was the solution
 async function init() {
   try {
     const answers = await promptUser();
     const readmeContent = generateREADME(answers);
 
-    // Write the generated README content to a file
     fs.writeFile('README.md', readmeContent, (err) => {
       if (err) throw err;
       console.log('Your New README.md File Has Been Successfully Generated');
